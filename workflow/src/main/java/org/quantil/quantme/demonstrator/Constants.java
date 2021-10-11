efbd5509cd80eeb72fd3be4d17025b63d5f106e8
wederbn <benjamin.weder@iaas.uni-stuttgart.de>
2021-10-11 11:10:13 +0200

    Finalize workflow until MDS

--- workflow/src/main/java/org/quantil/quantme/demonstrator/Constants.java
+++ workflow/src/main/java/org/quantil/quantme/demonstrator/Constants.java
@@ -18,6 +18,7 @@ public class Constants {
     public static final String VARIABLE_NAME_INPUT_DATA_FILE = "inputDataFile";
     public static final String VARIABLE_NAME_DISTANCES_FILE = "distancesFile";
     public static final String VARIABLE_NAME_ENTITY_DISTANCES_FILE = "entityDistancesFile";
+    public static final String VARIABLE_NAME_ENTITY_POINTS_FILE = "entityPointsFile";
 
     // URL snippets
     public static final String URL_SIM_TO_DIST = "/plugins/sim-to-dist-transformers@v0-1-0/process/";
@@ -27,16 +28,17 @@ public class Constants {
     public static final String URL_VISUALIZATION = "/plugins/visualization@v0-1-0/process/";
 
     // constants for the plugins
-    public static final String PARAMETER_DISTANCE_ATTRIBUTES = "dominanteFarbe";
+    public static final String PARAMETER_DISTANCE_ATTRIBUTES = "dominanteFarbe\ndominanterZustand\ndominanteCharaktereigenschaft\ndominanterAlterseindruck\ngenre";
     public static final String PARAMETER_DISTANCE_TRANSFORMER = "linear_inverse";
     public static final String PARAMETER_AGGREGATION_AGGREGATOR = "mean";
     public static final String PARAMETER_DEFAULT_DIMENSIONS = "2";
     public static final String PARAMETER_DEFAULT_MAX_ITER = "300";
     public static final String PARAMETER_DEFAULT_NINIT = "4";
-    public static final String PARAMETER_MDS_METRIC = "Metric MDS";
+    public static final String PARAMETER_MDS_METRIC_DEFAULT = "metric_mds";
     public static final String RESPONSE_TASK_RESULT_URL = "taskResultUrl";
     public static final String RESPONSE_ATTRIBUTE_DISTANCE_FILE = "attr_dist.zip";
     public static final String RESPONSE_ENTITY_DISTANCE_JSON = "entity_distances.json";
+    public static final String RESPONSE_ENTITY_POINTS_JSON = "entity_points.json";
     public static final String RESPONSE_OUTPUTS = "outputs";
     public static final String RESPONSE_OUTPUTS_NAME = "name";
     public static final String RESPONSE_OUTPUTS_HREF = "href";
@@ -46,5 +48,10 @@ public class Constants {
     public static final String REQUEST_TRANSFORMER = "transformer";
     public static final String REQUEST_SIMILARITIES = "attributeSimilaritiesUrl";
     public static final String REQUEST_DISTANCES = "attributeDistancesUrl";
+    public static final String REQUEST_DIMENSIONS = "dimensions";
+    public static final String REQUEST_N_INIT = "nInit";
+    public static final String REQUEST_MAX_ITER = "maxIter";
+    public static final String REQUEST_MDS_METRIC = "metric";
+    public static final String REQUEST_ENTITY_DISTANCES = "entityDistancesUrl";
     public static final String STATUS_SUCCESS = "SUCCESS";
 }
